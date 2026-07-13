74ec4c7c11336b80a018c1f21d030fa77869207e
Ivan Sokolskiy <gladtycoon@gmail.com>
2026-07-13 17:14:44 +0200

    add fixes

--- lms/tasks.py
+++ lms/tasks.py
@@ -14,20 +14,18 @@ def send_email_about_update_course(course_id):
     """Отправляет письмо об обновлении курса (создании в курсе нового урока)."""
 
     course = Course.objects.get(id=course_id)
-    subscribers = Subscription.objects.filter(course=course)
+    subscribers = Subscription.objects.filter(course=course).select_related("users")
     subject = f"Обновление материалов курса {course.name}"
     message = f"В курсе {course.name} появился новый урок!"
     email_list = []
     if course.last_updated and now() - course.last_updated < timedelta(hours=4):
         return "Курс обновлялся менее 4 часов назад, письмо не отправлено"
-    else:
-        for sub in subscribers:
-            email_list.append(sub.user.email)
-            # if sub.user.tg_chat_id:                                 - для отправки сообщения в тлг
-            #     send_telegram_message(sub.user.tg_chat_id, message) - еще нужно добавить поле tg_chat_id в модель User
-        if email_list:
-            send_mail(subject, message, EMAIL_HOST_USER, email_list)
-            course.last_updated = (
-                timezone.now()
-            )  # для сброса начала отсчета четырех часов
-            course.save()
+    email_list = [sub.user.email for sub in subscribers]
+    # for sub in subscribers:
+    #     email_list.append(sub.user.email)
+    # if sub.user.tg_chat_id:                                 - для отправки сообщения в тлг
+    #     send_telegram_message(sub.user.tg_chat_id, message) - еще нужно добавить поле tg_chat_id в модель User
+    if email_list:
+        send_mail(subject, message, EMAIL_HOST_USER, email_list)
+        course.last_updated = timezone.now()  # для сброса начала отсчета четырех часов
+        course.save()
